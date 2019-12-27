54c0c073a6bc0e681a92cc3ebd9b2186f02d21f5
andysnake <andrea.diiorio,sniper@gmail.com>
2019-12-27 21:53:15 +0100

    graph_utils slightly simplified fully retested , only single rappresentation on adj list, list of edges and inc matrix supported on the fly. coloring in graph GUI, Grap class <-> pl model infinity first trivial support

--- graph_utils.py
+++ graph_utils.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python2.7
 #utils classes and functions used by the min cut problem solver
-
-import copy,random
+import copy
+from random import choice,seed
 
 #basic graph rappresentation by Adjacency Lists and python built-in data types
 #Edges rappresentation via tuple (naming reflect this: edge e: 1->2; 1 is the tail of the edge
 EDGE_TAIL_INDEX=0	#edge tail index in edge tuple
 EDGE_HEAD_INDEX=1	#edge head index in edge tuple
 EDGE_WEIGHT_INDEX=2	#edge weight index
+DEFAULT_EDGE_WEIGHT=1
 class Graph:
 	def __init__(self,**argsDict):
 	#non default init of graph by calling the constructor with kwargs named in same way of graph class attributes
 	#it's assumed here but in the whole module consistency in the passed parameters, like edge correspond to nodes
 		self.directed=False
 		self.nodes=dict()	#adjacency list rappresentation of graph nodeID->list of nodesID reachable 
-		self.edges=list()	#list of tuples [edgeTail,edgeHead]
 		#non default initialization
 		if argsDict.get("directed")!=None:
 			self.nodes=argsDict["directed"]
@@ -25,6 +25,7 @@ class Graph:
 	
 	def addNodes(self,nodesNew):
 		#add new nodes as dict of nodesID -> nodesID reachable from this node
+		#supported with type check also list of nodesID initialized with empty adjacent list 
 		if type(nodesNew)==type(dict()):
 			self.nodes.update(nodesNew)
 		elif type(nodesNew)==type(list()): #otherwise add empty nodes (un connected) from passed list nodesNew
@@ -35,20 +36,21 @@ class Graph:
 
 	def addEdges(self,edgesNew):
 		#edge to add to graph are rappresented by a list of tuples of newly (ordinatlly) connected nodes, e.g. [(1,2),(4,1)
-		print("\n",edgesNew,"\n",self.nodes,"\n",self.edges)
-		#update edges fields
-		self.edges.extend(edgesNew)
+		# print(self.nodes)
+		# print("adding edges:",edgesNew)
 		#update adj list in place
 		for e in edgesNew:
+			#eNodeTail=e[EDGE_TAIL_INDEX]
+			#eNodeHead=e[EDGE_HEAD_INDEX]
+
 			self.nodes[e[EDGE_TAIL_INDEX]].append(e[EDGE_HEAD_INDEX])
 			if not self.directed:	#update adj list for un directed graphs
 				self.nodes[e[EDGE_HEAD_INDEX]].append(e[EDGE_TAIL_INDEX])
-		print(self.nodes)
+		# print(self.nodes)
 	
 	def remEdges(self,edgesToRemove):
 		#edgesToRemove is a list of tuple of edges to remove
 		for edge in edgesToRemove:
-			self.edges.remove(edge)
 			srcNodeID=edge[EDGE_TAIL_INDEX]
 			dstNodeID=edge[EDGE_HEAD_INDEX]
 			#update src node adj list
@@ -77,26 +79,57 @@ class Graph:
 						edgesToRemove.append((node2,node)) #backward edge to remove
 		self.remEdges(edgesToRemove)
 
-	def build_incidentMatrix(self):
-		print(self.nodes)
-		print(self.edges)
+	def extractEdges(self):
+		#extract edges from graph in form of list of tuples
+		#like (TAIL,HEAD,WEIGHT)
+		edges = list()
+		for n, neigh in list(self.nodes.items()):
+			for n1 in neigh:
+				edge=(n, n1, DEFAULT_EDGE_WEIGHT)
+				if n==SUPER_SOURCE_NODE_ID or n1==SUPER_DEST_NODE_ID:
+					edge=(n, n1, float("inf"))
+				edges.append(edge)
+		return edges
+
+	def build_incidentMatrix(self,printFlag=True):
 		EDGE_EXIT_NODE=1
 		EDGE_ENTER_NODE=-1
+		ss_col_index=-2
+		sd_col_index=-1
+		#build edges list on the fly
+		edges=self.extractEdges()
 		#0 init matrix shallow copy fully avoiding
 		incidentMatrix=list()
 		for i in range(len(self.nodes)):
 			incidentMatrix.append(list())
-			for j in range(len(self.edges)):
+			for j in range(len(edges)):
 				incidentMatrix[i].append(0)
 
-		for edgeID in range(len(self.edges)):
-			edgeTuple=self.edges[edgeID]
+		edges.sort()		#TODO EASIER TO READ
+		for edgeID in range(len(edges)):
+			edgeTuple=edges[edgeID]
 			edgeSrc=edgeTuple[EDGE_TAIL_INDEX]
 			edgeDst=edgeTuple[EDGE_HEAD_INDEX]
+			#handle super source and super dest nodes in incident matrix
+			if edgeSrc == SUPER_SOURCE_NODE_ID:
+				edgeSrc=ss_col_index
+			if edgeDst == SUPER_DEST_NODE_ID:
+				edgeDst=sd_col_index
+
 			incidentMatrix[edgeSrc][edgeID]=EDGE_EXIT_NODE
 			incidentMatrix[edgeDst][edgeID]=EDGE_ENTER_NODE
 			if self.directed==False:			#handle non orienthed graph
 				incidentMatrix[edgeDst][edgeID]=EDGE_EXIT_NODE
+
+		if(printFlag):		#print incident matrix infos
+			print("---nodes---")
+			print(self.nodes)
+			print("---edges---")
+			print(edges)
+			print("edges IDs  ", list(range(len(edges))))
+			for r in incidentMatrix:
+				print(r)
+			print("\n\n")
 		return incidentMatrix
 				
 
@@ -108,14 +141,34 @@ class Graph:
 		directedGraph=copy.deepcopy(self)
 		directedGraph.directed=True
 		edgesToAdd=list()
-		#create reverse edges with same cost to add to undirect graph 
-		for edge in directedGraph.edges:
-			newEdge=(edge[EDGE_HEAD_INDEX],edge[EDGE_TAIL_INDEX],edge[EDGE_WEIGHT_INDEX])
-			edgesToAdd.append(newEdge)
-		
-		directedGraph.addEdges(edgesToAdd)
+		#  #create reverse edges with same cost to add to undirect graph
+		# for edge in directedGraph.EDGEEE:
+		# 	newEdge=(edge[EDGE_HEAD_INDEX],edge[EDGE_TAIL_INDEX],edge[EDGE_WEIGHT_INDEX])
+		# 	if newEdge not in directedGraph.EDGEEE: #undirect graph already have double directed edges
+		# 		edgesToAdd.append(newEdge)
+		#
+		# directedGraph.addEdges(edgesToAdd)
 		return directedGraph
-		
+
+#special ID for terminal nodes
+SUPER_SOURCE_NODE_ID=-83
+SUPER_DEST_NODE_ID=-84
+
+def addTerminalNodes(graph):
+	#add terminal nodes to a directed graph, a super source and a source sink (S*,T*)
+	#these special nodes will be connected with every other nodes in the graph with an edge s,v or u,t of inifinity cost (python float('inf'))
+	#also defined special 
+	terminalNodesToAdd=[SUPER_SOURCE_NODE_ID,SUPER_DEST_NODE_ID]
+	edgesToAdd=list()
+	for nodeID in graph.nodes:
+		newEdge_sv=(SUPER_SOURCE_NODE_ID,nodeID,float("inf")) #fake edge between fake super source and other nodes
+		newEdge_ut=(nodeID,SUPER_DEST_NODE_ID,float("inf")) #fake edge between fake super source and other nodes
+		edgesToAdd.append(newEdge_sv)
+		edgesToAdd.append(newEdge_ut)
+	graph.addNodes(terminalNodesToAdd)
+	graph.addEdges(edgesToAdd)
+	
+
 
 
 def genTreePath(nodesN):
@@ -124,34 +177,221 @@ def genTreePath(nodesN):
 	treeNodesIDs=list(range(nodesN))
 	treeEdges=list()
 	nodesDeepCopy=copy.deepcopy(treeNodesIDs)
-	node=random.choice(nodesDeepCopy)	#select random root among nodes IDs
+	node=choice(nodesDeepCopy)	#select random root among nodes IDs
 	nodesDeepCopy.remove(node)
 
 	for x in range(TREE_SIZE-1):
-		nextNode=random.choice(nodesDeepCopy)
+		nextNode=choice(nodesDeepCopy)
 		nodesDeepCopy.remove(nextNode)
 
-		edge=(node,nextNode,1)
+		edge=(node,nextNode,DEFAULT_EDGE_WEIGHT)
 		treeEdges.append(edge)
 		node=nextNode
 	return (treeNodesIDs,treeEdges)
 
-def _printMatrix(matrix,title):
-	print(title)
-	for row in matrix:
-		print(row)
-if __name__=="__main__":
-	random.seed()
-	#Basic graphs build
-	#tree
-	TREE_SIZE=10
+def pruferCodeGenTreeEdges(pruferCodeArr):
+	#from a prufer code in pruferCodeArr generate a list of edges rappresenting a tree returned
+	#basic implementation taken from wikipedia
+	#robstuness check on validity of prufer code passed
+	if type(pruferCodeArr)!=type(list()) or  any([i > len(pruferCodeArr) for i in pruferCodeArr]):
+		raise Exception("invalid prufer code")
+	treeEdges=list()
+	nodes=list(range(len(pruferCodeArr)+2))
+	degrees=[1]*len(nodes)
+	for f in pruferCodeArr:
+		degrees[f]+=1
+	for nodeF in pruferCodeArr:
+		for node in nodes:
+			if degrees[node] == 1:
+				treeEdges.append((nodeF,node,DEFAULT_EDGE_WEIGHT))
+				degrees[nodeF]-=1
+				degrees[node]-=1
+				break
+			#else:	#debug
+			#	print(degrees,node)
+	#last edge to add u->v
+	u,v=0,0
+	for node in nodes:
+		if degrees[node]==1:
+			if u==0:
+				u=node
+			else:
+				v=node
+				break
+	treeEdges.append((u,v,DEFAULT_EDGE_WEIGHT))
+	degrees[u]-=1
+	degrees[v]-=1
+	return treeEdges
+
+def pruferCodeGenTree(treeSize=1,logPrint=False):
+	#generate a random prufer code array and corresponding tree 
+	#return graph generated
+
+	newNodesArrIDs=list(range(treeSize-2))	#support array
+	newNodes=list(range(treeSize))		#new nodes for the random prufer code tree
+	pruferCodeArr=list()			#actual prufer code array
+	#build prufer code arr TODO CONSTRAINT FOR BETTER QUALITY ?
+	for n in range(len(newNodesArrIDs)):
+		nid=choice(newNodesArrIDs)
+		pruferCodeArr.append(nid)
+		newNodesArrIDs.remove(nid)
+
+	newEdges=pruferCodeGenTreeEdges(pruferCodeArr)
+	pruferTreeGraph=Graph(nodes=newNodes,edges=newEdges)
+	if logPrint:
+		print("prufer code from random array: ",pruferCodeArr,"resulting edges")
+		print(newEdges)
+		pruferTreeGraph.build_incidentMatrix()
+	return pruferTreeGraph
+
+def graphTickennerRand(graph,nodesN,edgesN,MAX_TRIES_NUM=20):
+	#make the input graph "tickenner" by adding nodesN and edgesN randomly
+	#return tuple of ( newly added nodes num, newly added edges num )
+
+	#Adding nodes
+	firstNewNodeID=1+max(list(graph.nodes.keys()))
+	newNodesIDs=list(range(firstNewNodeID,firstNewNodeID+nodesN))
+	graph.addNodes(newNodesIDs)
+
+	#adding edges
+	#for a limited num of times, try select a pair of nodes and add edge if not already exist
+	newEdgesToAdd=list()
+	nodesIDs=list(graph.nodes.keys())
+	triesLeft=MAX_TRIES_NUM
+	while triesLeft >= 0 and edgesN>=0:
+		edgeTailNode=choice(nodesIDs)
+		edgeHeadNode=choice(nodesIDs)
+		newPossibleEdge=(edgeTailNode,edgeHeadNode,DEFAULT_EDGE_WEIGHT)
+		#eventually new possible edge may not be added to the graph
+		edgeAlreadyIn=edgeHeadNode in graph.nodes[edgeTailNode] or edgeTailNode==edgeHeadNode or newPossibleEdge in newEdgesToAdd
+		if edgeAlreadyIn:
+			triesLeft-=1
+			continue
+
+		graph.addEdges([(edgeTailNode,edgeHeadNode,DEFAULT_EDGE_WEIGHT)])	#single insert
+
+	#batch insert
+	#newEdgesToAdd.append((edgeTailNode,edgeHeadNode,DEFAULT_EDGE_WEIGHT))
+	#graph.addEdges(newEdgesToAdd)
+	print("Added ",len(newNodesIDs)," nodes and ",len(newEdgesToAdd)," edges to the graph")
+	print(graph.nodes)
+	return (len(newNodesIDs),len(newEdgesToAdd))
+		
+
+#####	GRAPH WRITE VIEW LOGIC ON GUI OF NETWORKX
+import matplotlib.pyplot as plt
+import networkx as nx
+drawn=0
+def drawGraph(graph):
+
+	#try draw graph by nxgraph python lib
+	#return nx.Graph obj 
+	global drawn
+
+	g=nx.Graph()	
+	if graph.directed:	#overwrite  g for direct graphs
+		g=nx.DiGraph()
+	g.add_nodes_from(list(graph.nodes.keys()))
+	edgesHeadTailList=list()
+	edgesWeightList=list()		#same indexing edgesHeadTailList abve
+	edges=graph.extractEdges()
+	for e in edges:
+		edgesHeadTailList.append(e[:-1]) #exclude nested weight field
+		edgesWeightList.append(e[-1])
+	g.add_edges_from(edgesHeadTailList)
+	plt.subplot()
+	nx.draw(g,with_labels=True)
+	drawn += 1
+	print("drawn", drawn)
+	plt.show()
+	return g
+
+# g.add_edge(e[EDGE_TAIL_INDEX],e[EDGE_HEAD_INDEX],color=colo,weight=e[EDGE_WEIGHT_INDEX])
+
+def drawGraphColored(graph):
+
+	#try draw graph by nxgraph python lib
+	#graph drawn with colors
+	#return nx.Graph obj
+	global drawn
+
+	g=nx.Graph()
+	if graph.directed:	#overwrite  g for direct graphs
+		g=nx.DiGraph()
+	nodesIDSorted=list(graph.nodes.keys())
+	nodesIDSorted.sort()
+	g.add_nodes_from(nodesIDSorted)
+	edgesHeadTailList=list()
+	fakeEdges=list()
+	edgesWeightList=list()		#same indexing edgesHeadTailList abve
+	edges = graph.extractEdges()
+	for e in edges:
+		edgesHeadTailList.append(e[:-1])  # exclude nested weight field
+		edgesWeightList.append(e[-1])
+		if e[EDGE_WEIGHT_INDEX]==float("inf"):
+			fakeEdges.append(e)
+
+	g.add_edges_from(edgesHeadTailList)
+
+	plt.subplot()			#like a base canva
+	nodesPositions = nx.spring_layout(g)  # positions for all nodes
+	# nodesPositions = nx.shell_layout(g)  # positions for all nodes
+	firstTrueNodeIndex=nodesIDSorted.index(SUPER_SOURCE_NODE_ID)+1
+	# nx.draw_networkx_nodes(g,nodesPositions,nodesIDSorted[firstTrueNodeIndex:],node_color="g")
+	# nx.draw_networkx_nodes(g,nodesPositions,nodesIDSorted[:firstTrueNodeIndex],node_color="b")
+	# nx.draw_networkx_edges(g,nodesPositions,edgesHeadTailList,edge_color="b")
+
+	#MOST STRAIGHTFORWARD DRAWING APPROCH: start basic "dflt" draw, then re-draw different colors stuff (e.g. fake edges)
+	nx.draw_networkx(g,nodesPositions)
+	nx.draw_networkx_edges(g,nodesPositions,fakeEdges,edge_color="b")
+	drawn += 1
+	print("drawn", drawn)
+	plt.show()
+	return g
+
+if __name__=="__main__":  	#TODO debug switch  see below
+#def main():			#TODO NEEDED PYTHON SHELL TEST TMP
+	global TREE_SIZE,gTreePruferExtended,gPathExtended
+	seed()
+	TREE_SIZE=4
+	
+	###	Build Rand Graph from a random Path tickened randomly 
 	treeNodes,treeEdges=genTreePath(TREE_SIZE)
 	treePath=Graph(nodes=treeNodes,edges=treeEdges)
-	_printMatrix(treePath.build_incidentMatrix(),"incidentMatrix basic treePath")
+	print("treePath simple")
+	treePath.build_incidentMatrix()
+	newNodes,newEdges=graphTickennerRand(treePath,0,1)
+	#print("newNodes",newNodes,"newEdges",newEdges)
+	print("treePath tickened ")
+	treePath.build_incidentMatrix()
+
 	treePathOrientized=treePath.directizeGraph()
-	_printMatrix(treePathOrientized.build_incidentMatrix(),"incidentMatrix orientized treePath")
+	print("treePath tickened directized")
+	treePathOrientized.build_incidentMatrix()
+	gPathExtended=drawGraph(treePathOrientized)
+	
+	
+	### Build Rand Graph from prufer code tree generation tickened randomly 
+	pruferTreeGraph=pruferCodeGenTree(TREE_SIZE,True)
+	print("prufer code tree")
+	pruferTreeGraph.build_incidentMatrix()
+	newNodes,newEdges=graphTickennerRand(pruferTreeGraph,0,2)
+	print("prufer code tree tickened")
+	pruferTreeGraph.build_incidentMatrix()
+	gTreePruferExtended=drawGraph(pruferTreeGraph)
+
+	pruferTreeGraphDirectized= pruferTreeGraph.directizeGraph()
+	print("prufer code tree tickened directized")
+	pruferTreeGraphDirectized.build_incidentMatrix()
+
+	addTerminalNodes(pruferTreeGraphDirectized)
+	print("prufer code tree tickened directized with terminal nodes")
+	pruferTreeGraphDirectized.build_incidentMatrix()
+	drawGraphColored(pruferTreeGraphDirectized)
+#main()		#TODO SWITHCK, see above 
+
+
 
 	#TODO TESTS def & run
-	#TODO FROM MINIMAL TREE PROGRSSIVELLY ADD EDGES -> ticken function
 	#TODO SIMPLE ITERATION -> test over 2-nested iterations	for variadic num nodes -> increase edge num from start point (ticken func over minimal tree)
 	#	-> sara base per test e confronti da mettere nella relazione
--- mincut_lp.py
+++ mincut_lp.py
@@ -1,3 +1,88 @@
 #!/usr/bin/env python2.7
 
 #implemementation of solver for min cut problem over a generic undirected,connected graph using gurobi
+from gurobipy import *
+from graph_utils import *
+import re
+
+GRB_INFINITY=9 #TODO GUROBYPY SUPPERT ?
+def solveMinCut(nodes,edges): 	
+	
+	#solve min cut problem over graph
+	#graph has to be directed and with super source and super sink connected with all nodes with inifity cost edges TODO SET IDENTIFIERS FOR SPECIAL EDGES-SSOURCE-SSINK
+	#returned sol: selected edges for the min cut ( subset of input ones)
+	global mod		#TODO DEBUG
+	mod=Model("minCut")
+	#Variables
+	zv=mod.addVars(nodes)	#TODO? EXCLUDED TERMINAL NODES 
+	d_uv=mod.addVars(len(edges))
+	edgesCostVector=list()			#costant vector for obj func
+	#Constraints
+	for e in range(len(edges)):
+		edge=edges[e]
+		#take edge metadata
+		edgeSrcNodeID=edge[EDGE_TAIL_INDEX]
+		edgeDstNodeID=edge[EDGE_HEAD_INDEX]
+		edgeW=edge[EDGE_WEIGHT_INDEX]
+		if edgeW == float("inf"): edgeW=GRB_INFINITY
+		edgesCostVector.append(edgeW)
+		if edgeSrcNodeID==SUPER_SOURCE_NODE_ID: 	#s,v edge
+			mod.addConstr( d_uv[e] + zv[edgeSrcNodeID] >= 1 )
+		elif edgeDstNodeID==SUPER_DEST_NODE_ID:	#u,t edge  
+			mod.addConstr( d_uv[e] - zv[edgeDstNodeID] >= 0 )
+		else:					#real edge
+			mod.addConstr( d_uv[e] - zv[edgeSrcNodeID] + zv[edgeDstNodeID] >= 0 )
+
+	#Constraints signs
+	mod.addConstrs((d_uv[e] >= 0 for e in range(len(d_uv))),name="signEdges")
+	#mod.AddConstraints(node[n]  #TODO SOTTOINTESO APPARTENENETE A R ?
+
+	#Objective function
+	mod.setObjective(( sum(d_uv[e]*edgesCostVector[e] for e in range(len(edges)))),GRB.MINIMIZE)
+	#gurobi solve
+	mod.write("minCutBeforeOptimize.lp")
+	mod.optimize()
+	mod.write("minCutAfterOptimize.lp")
+	#printSolution(mod)
+	selectedEdges=list()	#output soulution egdges 
+	for edgeVarKey,edgeVarV in d_uv.items():
+		if edgeVarV.X >0.0:		#selected edge from solution values of model decision var
+			#extract decision var index (that match with the input edge's one) with a regex according standard naming scheme gurobi (as said in gurobi ref manual)
+			edgeVarIndex=int(re.findall(".+\[(.*)\]",edgeVarV.VarName)[0]) 
+			#TODO ALSO DIRECTLY WITH _colno ? gurobi ref manul not talk about indexing 
+			selectedEdges.append(edges[edgeVarIndex])	#append selected edge by same indexing input edges <-> decision var
+
+	return selectedEdges
+
+#def printSolution(model):	#TODO PRINT SOL TEMPLATE FROM DIET MODEL
+#    if model.status == GRB.Status.OPTIMAL:
+#        print('\nCost: %g' % model.objVal)
+#        buyx = model.getAttr('x', buy)
+#        for f in foods:
+#            if buy[f].x > 0.0001:
+#                print('%s %g' % (f, buyx[f]))
+#    else:
+#        print('No solution')
+
+#if __name__=="__main__":		#TODO DEBUG SWITCH
+def main():				#TODO DEBUG SWITCH
+	global randGraphDir,pruferTreeGraph
+
+	#Generate Tree from a random prufer code arr, then ticken the tree randomly 
+	TREE_SIZE=3
+	pruferTreeGraph=pruferCodeGenTree(TREE_SIZE,True)
+	newNodes,newEdges=graphTickennerRand(pruferTreeGraph,0,5)
+	#adding terminal nodes on directized graph
+	randGraphDir=pruferTreeGraph.directizeGraph()
+	addTerminalNodes(randGraphDir)
+	print("tickened prufer code tree with terminal nodes ")
+	randGraphDir.build_incidentMatrix()
+	gTreePruferExtended=drawGraphColored(randGraphDir)
+
+	#PL min cut version
+	minCutEdges=solveMinCut(list(randGraphDir.nodes.keys()),randGraphDir.extractEdges())
+	print(minCutEdges)
+	#directize G
+	#add fake super source,super sink
+	#pass G nodes,edges to solve function to compute min cut
+
